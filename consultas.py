d576ba4d24bf284e2e2dfd14178be787a5fb9756
Sena2K <senamurilo2@gmail.com>
2024-10-09 13:50:39 -0300

    first commit

--- consultas.py
+++ consultas.py
@@ -13,71 +13,7 @@ class ConsultasPokemon:
         for registro in resultado:
             print(f"{registro['nome']} - Peso: {registro['peso_kg']} kg")
 
-    @staticmethod
-    def _criar_fraquezas_tipo(tx):
-        fraquezas = [
-            ('Normal', 'Fighting'),
-            ('Normal', 'Ghost'),
-            ('Normal', 'Dark'),
-            ('Fire', 'Water'),
-            ('Fire', 'Ground'),
-            ('Fire', 'Rock'),
-            ('Water', 'Electric'),
-            ('Water', 'Grass'),
-            ('Electric', 'Ground'),
-            ('Grass', 'Fire'),
-            ('Grass', 'Ice'),
-            ('Grass', 'Poison'),
-            ('Grass', 'Flying'),
-            ('Grass', 'Bug'),
-            ('Ice', 'Fire'),
-            ('Ice', 'Fighting'),
-            ('Ice', 'Rock'),
-            ('Ice', 'Steel'),
-            ('Fighting', 'Psychic'),
-            ('Fighting', 'Flying'),
-            ('Fighting', 'Fairy'),
-            ('Poison', 'Psychic'),
-            ('Poison', 'Ground'),
-            ('Ground', 'Water'),
-            ('Ground', 'Grass'),
-            ('Ground', 'Ice'),
-            ('Flying', 'Electric'),
-            ('Flying', 'Ice'),
-            ('Flying', 'Rock'),
-            ('Psychic', 'Bug'),
-            ('Psychic', 'Ghost'),
-            ('Psychic', 'Dark'),
-            ('Bug', 'Fire'),
-            ('Bug', 'Flying'),
-            ('Bug', 'Rock'),
-            ('Rock', 'Water'),
-            ('Rock', 'Grass'),
-            ('Rock', 'Fighting'),
-            ('Rock', 'Ground'),
-            ('Rock', 'Steel'),
-            ('Ghost', 'Ghost'),
-            ('Ghost', 'Dark'),
-            ('Dragon', 'Ice'),
-            ('Dragon', 'Dragon'),
-            ('Dragon', 'Fairy'),
-            ('Dark', 'Fighting'),
-            ('Dark', 'Bug'),
-            ('Dark', 'Fairy'),
-            ('Steel', 'Fire'),
-            ('Steel', 'Fighting'),
-            ('Steel', 'Ground'),
-            ('Fairy', 'Poison'),
-            ('Fairy', 'Steel'),
-            ('Fairy', 'Fire'),
-            ('Fairy', 'Dark'),
-            ('Fairy', 'Fighting')
-        ]
-        for tipo1, tipo2 in fraquezas:
-            tx.run("""
-            MATCH (t1:Tipo {nome: $tipo1}), (t2:Tipo {nome: $tipo2})
-            MERGE (t1)-[:FRACO_CONTRA]->(t2)
-            """, tipo1=tipo1, tipo2=tipo2)
+
 
     @staticmethod
     def _consulta_pikachu_fraqueza(tx):
